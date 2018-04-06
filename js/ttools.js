952fb6b365f1e974c35ac086138df07d5417db32
benoit <benoit@atmire.com>
2018-04-06 09:50:25 +0200

    New attempt at multifunctions - using Promise.all

--- js/ttools.js
+++ js/ttools.js
@@ -5,6 +5,8 @@ var HOURGLASS_ICON = 'https://benoit-atmire.github.io/ttools/img/hourglass.png';
 var CLOCK_ICON_WHITE = 'https://benoit-atmire.github.io/ttools/img/clock_white.png';
 var HOURGLASS_ICON_WHITE = 'https://benoit-atmire.github.io/ttools/img/hourglass_white.png';
 
+var Promise = TrelloPowerUp.Promise;
+
 TrelloPowerUp.initialize({
   /*'card-buttons': function(t, options){
     return [{
@@ -124,7 +126,57 @@ function getAllBadges(t) {
 
     return badges;
 */
-   return t.card('all')
+   return Promise.all([t.card('all'), t.getAll()])
+        .then(function (values) {
+            var card = values[0];
+            var data = values[1];
+            var today = new Date();
+            var creation = new Date(1000*parseInt(card.id.substring(0,8),16));
+            var lastUpdate = new Date(card.dateLastActivity);
+            var daysSinceCreation = Math.round(Math.abs((today.getTime() - creation.getTime())/(24*60*60*1000)));
+            var daysSinceUpdate = Math.round(Math.abs((today.getTime() - lastUpdate.getTime())/(24*60*60*1000)));
+
+            var w2plink = data.card.shared.w2plink;
+            var gitlablink = data.card.shared.gitlablink;
+
+            var badges = [{
+                  icon: daysSinceCreation < 15 ? CLOCK_ICON : CLOCK_ICON_WHITE,
+                  text: daysSinceCreation + " day" + (daysSinceCreation < 15 ? "" : "s"),
+                  color: daysSinceCreation < 15 ? null : 'red',
+                  title: 'Open for'
+                },
+                {
+                  icon: daysSinceUpdate < 7 ? HOURGLASS_ICON : HOURGLASS_ICON_WHITE,
+                  text: daysSinceUpdate + " day" + (daysSinceUpdate < 7 ? "" : "s"),
+                  color: daysSinceUpdate < 7 ? null : 'red',
+                  title: 'Inactive for'
+                }
+            ];
+
+            if (w2plink && w2plink != "") {
+                badges.push({
+                    icon: W2P_ICON,
+                    text: 'W2P',
+                    url: w2plink,
+                    title: 'Task / Project'
+                });
+            }
+
+            if (gitlablink && gitlablink != "") {
+                badges.push({
+                    icon: GIT_ICON,
+                    text: 'Git',
+                    url: gitlablink,
+                    title: 'Branch / Commit'
+                });
+            }
+
+            return badges;
+
+        })
+
+
+   /*return t.card('all')
            .then(function (card) {
              var today = new Date();
              var creation = new Date(1000*parseInt(card.id.substring(0,8),16));
@@ -181,6 +233,6 @@ function getAllBadges(t) {
               );*/
 
 
-             return badges;
-           });
+             //return badges;
+           //});
 }
